f811449e96efc0f84e73918c85e8ad3326736fd1
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-10-31 23:07:07 +0600

    Updated Latest work section

--- src/assets/myWork_data.jsx
+++ src/assets/myWork_data.jsx
@@ -1,19 +1,19 @@
 import Southern from '../assets/southern-winds-lanscaping.webp'
 import OneTwoSix from '../assets/onetwosix.webp'
 import Sheehan from '../assets/sheehan.webp'
-import Renownded from '../assets/renownedfinishing.webp'
+import Noborup from '../assets/Best_Sell.jpg'
 import project6_img from '../assets/project_6.png'
 import BrainyBox from '../assets/brainyboxbd.webp'
 
 
 
 const mywork_data = [
-    {
+   {
         w_no:1,
-        url: 'https://onetwosixdesign.com/',
+        url: 'https://noborupclient.vercel.app/',
         github_url: '#',
-        w_name:"OneTwoSix",
-        w_img:OneTwoSix
+        w_name:"Noborup",
+        w_img:Noborup
     },
     {
         w_no:2,
@@ -22,12 +22,12 @@ const mywork_data = [
         w_name:"BrainyBox",
         w_img:BrainyBox
     },
-    {
+     {
         w_no:3,
-        url: 'https://renownedfinishing.ca/',
+        url: 'https://onetwosixdesign.com/',
         github_url: '#',
-        w_name:"Renownded Finishing",
-        w_img:Renownded
+        w_name:"OneTwoSix",
+        w_img:OneTwoSix
     },
 
     {
